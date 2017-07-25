917a61924d710f9f02e5d1d089dc4d2fcf8cca7a
Julie Ng <me@julie.io>
2017-07-25 12:53:22 +0200

    feat: add faker for random names

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import faker from 'faker'
 
 let web = express()
 const port = process.env.PORT || 3000
@@ -9,6 +10,12 @@ web.use(bodyParser.urlencoded({ extended: false }))
 
 web.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+web.post('/api/login', function (req, res) {
+  res.send({
+    fullname: faker.name.findName()
+  })
 })
 
 web.listen(port, function () {
