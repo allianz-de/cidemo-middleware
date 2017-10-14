dc69c64a31f322205f2b04d0f4e87ef8d86be444
Julie Ng <me@julie.io>
2017-10-14 18:03:56 +0200

    feat(proxy): proxy api/login to auth service

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import faker from 'faker'
+import proxy from 'http-proxy-middleware'
+
+const PORT = process.env.PORT || 3000
+const LOGIN_API = process.env.LOGIN_API
 
 let web = express()
-const PORT = process.env.PORT || 3000
-
 web.all('*', logRequests)
+web.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`)
+})
 
 web.use(bodyParser.json({ limit: '50mb' }))
 web.use(bodyParser.urlencoded({ extended: false }))
 
+//-- Public Routes
+
 web.get('/', function (req, res) {
   res.send('Hello World!')
-})
-
-web.post('/api/login', function (req, res) {
-  res.send({
-    fullname: faker.name.findName()
-  })
 })
 
 web.get('/api/random/history', function (req, res) {
@@ -27,9 +28,9 @@ web.get('/api/random/history', function (req, res) {
   })
 })
 
-web.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`)
-})
+// -- Proxies
+
+web.use('/api/login', proxy({ target: LOGIN_API }))
 
 function logRequests (req, res, next) {
   if (process.env.NODE_ENV !== 'test') {
