09a8d2f4384538b6f5d7dc119e763b50c59e00d9
Julie Ng <me@julie.io>
2017-10-16 21:53:42 +0200

    test: missing routes. placeholder for API endpoint

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -14,6 +14,34 @@ describe ('app', () => {
       })
     })
   })
+
+  describe ('GET /api/login', () => {
+    context('API_LOGIN is not set', () => {
+      it (`returns 504 status`, (done) => {
+        request.get(url('/api/login'), (error, response, body) => {
+          expect(response.statusCode).toBe(504)
+          done()
+        })
+      })
+    })
+
+    context('API_LOGIN is set', () => {
+      xit ('returns 200 status')
+    })
+  })
+
+  describe ('GET /api/random/history', () => {
+    it ('returns json', (done) => {
+      request.get(url('/api/random/history'), (error, response, body) => {
+        expect(response.statusCode).toBe(200)
+
+        let json = JSON.parse(response.body)
+        expect(json.fullname).toBeDefined()
+        expect(json.accountHistory).toBeDefined()
+        done()
+      })
+    })
+  })
 })
 
 function url (path = '') {
