719893f7adf7f2df60800f23eaf61aecd13f914b
Julie Ng <me@julie.io>
2018-06-20 21:00:21 +0200

    chore: always use semicolons

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,49 +1,49 @@
 // eslint-disable-next-line no-unused-vars
-import { app } from '~/app'
-import request from 'request'
+import { app } from '~/app';
+import request from 'request';
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 describe ('app', () => {
   describe ('GET /', () => {
     it (`returns 'Hello World!`, (done) => {
       request.get(url('/'), (error, response, body) => {
-        expect(response.statusCode).toBe(200)
-        expect(response.body).toBe('Hello World!')
-        done()
-      })
-    })
-  })
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe('Hello World!');
+        done();
+      });
+    });
+  });
 
   describe ('GET /api/login', () => {
     context('API_LOGIN is not set', () => {
       it (`returns 504 status`, (done) => {
         request.get(url('/api/login'), (error, response, body) => {
-          expect(response.statusCode).toBe(504)
-          done()
-        })
-      })
-    })
+          expect(response.statusCode).toBe(504);
+          done();
+        });
+      });
+    });
 
     context('API_LOGIN is set', () => {
-      xit ('returns 200 status')
-    })
-  })
+      xit ('returns 200 status');
+    });
+  });
 
   describe ('GET /api/random/history', () => {
     it ('returns json', (done) => {
       request.get(url('/api/random/history'), (error, response, body) => {
-        expect(response.statusCode).toBe(200)
+        expect(response.statusCode).toBe(200);
 
-        let json = JSON.parse(response.body)
-        expect(json.fullname).toBeDefined()
-        expect(json.accountHistory).toBeDefined()
-        done()
-      })
-    })
-  })
-})
+        let json = JSON.parse(response.body);
+        expect(json.fullname).toBeDefined();
+        expect(json.accountHistory).toBeDefined();
+        done();
+      });
+    });
+  });
+});
 
 function url (path = '') {
-  return `http://localhost:${port}` + path
+  return `http://localhost:${port}` + path;
 }
--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-import faker from 'faker'
-import proxy from 'http-proxy-middleware'
+import bodyParser from 'body-parser';
+import express from 'express';
+import faker from 'faker';
+import proxy from 'http-proxy-middleware';
 
-const PORT = process.env.PORT || 3000
-const LOGIN_API = process.env.LOGIN_API || 'http://placeholder/'
+const PORT = process.env.PORT || 3000;
+const LOGIN_API = process.env.LOGIN_API || 'http://placeholder/';
 
-let web = express()
-web.all('*', logRequests)
+let web = express();
+web.all('*', logRequests);
 web.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
 // -- Public Routes
 
 web.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 web.get('/api/random/history', function (req, res) {
   res.send({
     fullname: faker.name.findName(),
     accountHistory: faker.helpers.createCard().accountHistory,
-  })
-})
+  });
+});
 
 // -- Proxies
 
-web.use('/api/login', proxy({ target: LOGIN_API }))
+web.use('/api/login', proxy({ target: LOGIN_API }));
 
 // -- MUST load 'body-parser' after proxies
 
-web.use(bodyParser.json({ limit: '50mb' }))
-web.use(bodyParser.urlencoded({ extended: false }))
+web.use(bodyParser.json({ limit: '50mb' }));
+web.use(bodyParser.urlencoded({ extended: false }));
 
 function logRequests (req, res, next) {
   if (process.env.NODE_ENV !== 'test') {
-    console.log(`${req.method} ${req.path}`, req.body)
+    console.log(`${req.method} ${req.path}`, req.body);
   }
-  next()
+  next();
 }
